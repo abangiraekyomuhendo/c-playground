2457d3f71c5dbfa73a38c0d59e5b981c768bed04
nimmusiima <abangiraprecious@gmail.com>
2024-07-10 17:54:58 +0300

    arrays

---
+++ arrays.c
@@ -0,0 +1,17 @@
+# include studio.h>
+
+int main () {
+    /*
+    Syntax
+    datatype arrayName[arraySize]*/
+
+    int age[5];
+
+    //storing data in an array 
+     int val[4] = {1, 4, 6, 7}; //these values are called array elements
+
+
+    int items[4] = {2,4};
+    //here we had defined the array size to have elements, but have only put three. What will happen here is these C will assign 0 twice to fill the size ealier allocated.
+
+}
