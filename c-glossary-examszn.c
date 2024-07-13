ed25beae3b21c2f63e68d125bf37edeca762123c
nimmusiima <abangiraprecious@gmail.com>
2024-07-13 13:55:10 +0300

    added content again lol

--- c-glossary-examszn.c
+++ c-glossary-examszn.c
@@ -2,4 +2,7 @@
 Author: Abangira
 
 Note: The goal of this is to go through fundamental concepts of the entire semester as I prepare for exams.
+
+Content covered in this semester: 
+Arrays, For Loops, While Loops
 */
