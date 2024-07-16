d21ca5358a792cd75cbbd14e96bb773b47d95edd
nimmusiima <abangiraprecious@gmail.com>
2024-07-16 16:42:32 +0300

    more updates

--- c-glossary-examszn.c
+++ c-glossary-examszn.c
@@ -4,5 +4,25 @@ Author: Abangira
 Note: The goal of this is to go through fundamental concepts of the entire semester as I prepare for exams.
 
 Content covered in this semester: 
-Arrays, For Loops, While Loops, Functions, If-Else statements, Pointers, Relational Operators,
+Arrays, For Loops, While Loops, Functions, If-Else statements, Pointers, Relational Operators, Basic Structured Programming theory questions
 */
+
+#include <stdio.h>
+
+int main () {
+
+//Pointers
+
+int x, y;
+int *p; //What the asterisk does is it refers to the variable that pointer variable p is pointing to. 
+
+p = &x;
+*p = 4;
+x = 4;
+
+printf("%d", x);
+
+    return 0;
+}
+
+//Pointers
