d2f8be45a3da838ef47613e8bbbc3cb6ef137749
nimmusiima <abangiraprecious@gmail.com>
2024-07-26 02:01:48 +0300

    hbd-functions

--- functions.c
+++ functions.c
@@ -1,6 +1,8 @@
 //don't forget to add sierpinski precious
 /*
 A function is a small section of code that is executed when we call it. 
+Benefits: 
+1. if you think you are going to use some code more than once, use a functions and just call the function whenever you need them
 */
 
 
@@ -9,9 +11,13 @@ So, let's say you wanto sing "happy birthday" copying and pasting the same line 
 */
 
 #include <stdio.h>
-
+  
 //creating the function
 void birthday () {
+    printf("Happy birthday to you!\n");
+    printf("Happy birthday to you!\n");
+    printf("Happy birthday dear Precious!\n");
+    printf("Happy birthday to you!\n");
 
 }
 
@@ -20,11 +26,20 @@ int main () {
 
     /*Technically, you could do it this way: */
 
-    printf("Happy birthday to you!\n");
-    printf("Happy birthday to you!\n");
-    printf("Happy birthday dear Precious!\n");
-    printf("Happy birthday to you!");
+    // Beginning of highlighted code
 
-    /*But, let's utilize functions now: */
+    // printf("Happy birthday to you!\n");
+    // printf("Happy birthday to you!\n");
+    // printf("Happy birthday dear Precious!\n");
+    // printf("Happy birthday to you!");
+
+    //End of highlighted code
+
+    /*But, let's utilize functions now: 
+    To evoke the function created about the main, type the name of the function, parentheses. This will execute the code within the function once. To execute it more than once, call the function the number of times you want (3 verses in this case.)*/
+
+    birthday();
+    birthday();
+    birthday();
     return 0;
 }
