00dffca3b6861daf8450668f04f647b838205330
nimmusiima <abangiraprecious@gmail.com>
2024-07-26 01:45:11 +0300

    hbd-functions

--- functions.c
+++ functions.c
@@ -1,1 +1,17 @@
 //don't forget to add sierpinski precious
+/*
+A function is a small section of code that is executed when we call it. 
+*/
+#include <stdio.h>
+
+/*
+So, let's say you wanto sing "happy birthday" copying and pasting the same line of code because it is not good practice.
+*/
+
+int main () {
+
+    /*Technically, you could do it this way: */
+    
+
+    return 0;
+}
