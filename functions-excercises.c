bc21d431f9ee099971ac96c4bd1b71f7a719f4a9
nimmusiima <abangiraprecious@gmail.com>
2024-07-26 02:35:52 +0300

    practice

--- functions-excercises.c
+++ functions-excercises.c
@@ -11,11 +11,19 @@ void helloWorld() {
 }
 
 /*Exercise 2: Add Two Numbers
-Write a function that takes two integers as input and returns their sum. In your main function, call this function with two numbers and print the result.
+Write a function that takes two integers as input and returns their sum. In your main function, call this function with two numbers and print the result.*/
+
+int add(int a, int b) {
+    return a + b;
+}
+
+int multiply(int c, int d) {
+    return c * d;
+}
 
 
 
-Exercise 3: Find the Maximum of Two Numbers
+/*Exercise 3: Find the Maximum of Two Numbers
 Write a function that takes two integers and returns the larger of the two. Use this function in your main function to find and print the maximum of two given numbers.
 
 Exercise 4: Check Even or Odd
@@ -36,7 +44,56 @@ These exercises will help you practice defining and using functions, passing arg
 
 int main() {
     //Excercise one:
-    helloWorld();
+    // helloWorld();
+
+    //Excercise two:
+    // int result;
+    // result = add(3,4);
+    // printf("The sum and of integers a and b is %d", result);
+
+    //hmm, what if we tried to input user data
+    /*
+    int a, b, result;
+    printf("Input value a: ");
+    scanf("%d", &a);
+
+    printf("Input value b: ");
+    scanf("%d", &b);
+
+    result = add(a, b);
+    printf("The sum and of integers a and b is %d", result);
+    */
+
+    // ******** Multiplication ********
+
+    /*
+    int product;
+    product = multiply(4, 4);
+    printf("The product of 4 and 4 is: %d", product);
+    */
+
+   //Add user input
+    /*
+    int c, d, mult_result;
+    
+    printf("Value one: ");
+    scanf("%d", &c);
+    printf("Value two: ");
+    scanf("%d", &d);
+    
+    
+    mult_result = multiply(c, d);
+    printf("Product: %d", mult_result);
+    */ //It worked!
+
+
+    // ******** Division ********
+
+
+    
 
     return 0;
+
+
+    
 }
