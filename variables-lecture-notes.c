0c3aa3346c8ee11e023e8b795331c64b72fdbfff
nimmusiima <abangiraprecious@gmail.com>
2024-07-26 15:51:20 +0300

    variables practice

---
+++ variables-lecture-notes.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    // //data types being decalred and named
+    // int event; 
+    // char heat;
+    // float time;
+
+    // //values being assigned to these named data types 
+    // event = 5;
+    // heat = 'B';
+    // time = 27.25;
+    // printf("The winning time in heat %c\n", heat);
+    // printf("of event %d was %f\n", event, time);
+
+
+    //field width specifiers
+    /*
+    Explanation: 
+    - Field width specifiers are used to print values and strings in fixed field widths and to round float and values off. */
+
+    int age = 33;
+
+    printf("Age is %d.\n", age);
+    printf("Age is %2d.\n", age);
+    printf("Age is %3d.\n", age);
+    printf("Age is %4d.\n", age);
+    printf("Age is %5d.\n", age);
+    printf("Age is %6d.\n", age);
+
+
+    return 0;
+}
