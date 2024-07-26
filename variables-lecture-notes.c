4287cd000cdb93e6806cab051f6efe9e082086e1
nimmusiima <abangiraprecious@gmail.com>
2024-07-26 16:10:10 +0300

    more

--- variables-lecture-notes.c
+++ variables-lecture-notes.c
@@ -19,15 +19,25 @@ int main() {
     Explanation: 
     - Field width specifiers are used to print values and strings in fixed field widths and to round float and values off. */
 
-    int age = 33;
+    // int age = 33;
 
-    printf("Age is %d.\n", age);
-    printf("Age is %2d.\n", age);
-    printf("Age is %3d.\n", age);
-    printf("Age is %4d.\n", age);
-    printf("Age is %5d.\n", age);
-    printf("Age is %6d.\n", age);
+    // printf("Age is %d.\n", age);
+    // printf("Age is %2d.\n", age);
+    // printf("Age is %3d.\n", age);
+    // printf("Age is %4d.\n", age);
+    // printf("Age is %5d.\n", age);
+    // printf("Age is %6d.\n", age);
 
+    //Rounding off floats and doubles
+    /*
+    We add a "." followed by a value to indicate the number decimal point to print.
+    */
+
+   float roundOff = 5.867472;
+
+   printf("Round off to one decimal point: %.1f\n", roundOff);
+   printf("Round off to two decimal points: %.2f\n", roundOff);
+   printf("Round off to three decimal points: %.3f\n", roundOff);
 
     return 0;
 }
