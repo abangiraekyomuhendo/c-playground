c1554cae642c4eb53a5222f4fad0fa0caf6c3bde
nimmusiima <abangiraprecious@gmail.com>
2024-07-27 11:28:21 +0300

    changes

---
+++ arithmeticassignment.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+
+    /*
+    Let's say you want to print out:
+    total = total + number;
+    ...you can shorten it to be 
+    total += number;
+
+    here is a list off arithmetic assignments
+    Addition: += | a += 3 replaces a = a + 3
+    Subtraction: -=
+    Multiplication: *=
+    Division: /=
+    Remainder(modulus): %=
+    */
+
+    return 0;
+}
